0f2dd85211fabb957adae72c2b24228694cd45b6
mengqingyue <1294083463@qq.com>
2019-03-30 16:54:40 +0800

    删除没有用的页面

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/img/logo.png">
-    <div>
-      <el-row>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮</el-button>
-      </el-row>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    空页面
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+
 };
 </script>
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
 
