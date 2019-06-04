f72f06f15cf315e3d25921ea189e0d3456140612
mengqingyue <1294083463@qq.com>
2019-06-04 08:26:49 +0800

    添加cesium包

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    空页面
+    <div id="cesiumContainer"></div>
   </div>
 </template>
 
 <script>
-
+var Cesium = require("cesium/Cesium");
+require("cesium/Widgets/widgets.css");
 export default {
   name: "app",
-
+  data() {
+    return {
+      viewer: null
+    };
+  },
+  methods: {
+    init() {
+      this.viewer = new Cesium.Viewer("cesiumContainer");
+    }
+  },
+  mounted() {
+    this.init();
+  }
 };
 </script>
 <style lang="scss">
-
+// @import 'cesium/Widgets/widgets.css';
 </style>
 
